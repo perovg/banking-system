ef409694fee9f956e43b493b155342f978046f25
Sasha <aleksandra.n0vo2005@gmail.com>
2024-03-25 19:14:49 +0300

    Changed README.md, add docs

--- src/Server.java
+++ src/Server.java
@@ -6,14 +6,11 @@ public class Server {
     static HashMap<String, Bank> banks = new HashMap<>();
     static HashMap<String, Client> clients = new HashMap<>();
 
-
     public static void main(String[] args) {
         //Запускаем сервер
         //Принимаем и обрабатываем запросы клиентов.
     }
-
     //Функция запуска сервера. возвращает ip-адрес.
-
     //Функции обработки всех запросов
     //Создания счетов
     //Перевод, пополнение, снятие.
