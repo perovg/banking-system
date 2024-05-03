9ec0a3f974f602437e2e627aef4a242d70c86470
Александра Новожилова <aleksandra.n0vo2005@gmail.com>
2024-05-03 08:16:52 +0000

    Add AdminControllerTest.java

---
+++ src/test/java/com/pgm/nad/BankingSystem/AdminControllerTest.java
@@ -0,0 +1,16 @@
+package com.pgm.nad.BankingSystem.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class AdminControllerTest extends CommonMvcTest {
+    @Test
+    void mainCatalog() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/admin"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().errorCount(0))
+                .andExpect(view().name("mainAdminPage"));
+    }
+}
